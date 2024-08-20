fdd941f1897d5248fa4ed3871cd8396cb5e2154d
PaulDeveloper73 <kisakyepaul73@gmail.com>
2024-08-20 09:16:37 +0300

    Refactored the code base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/Footer";
-import AppRoutes from "./pages/AppRoutes";
-import Dashboard from "./components/Dashboard";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Dashboard />
-      <AppRoutes />
-      <Footer />
-    </BrowserRouter>
-  );
+  return null;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Gallery from "./components/Gallery.jsx";
-import VanApp from "./vans/pages/App.jsx"; //vans app
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <VanApp /> */}
-    <Gallery />
+    <App />
   </React.StrictMode>
 );
